25e3aca96962dc8f64537c0ee3ad705e195dcd13
Jorge Manuel Bello <jorgebell@gmail.com>
2025-05-04 10:03:07 -0300

    agregado pagina login

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\v1\PersonasController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check()
+        ? redirect()->route('dashboard')  // Si ya está logueado
+        : redirect()->route('login');     // Si no lo está
 });
 
-Route::get('/vista/departamentos', function () {
-    return view('departamentos.api-index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
