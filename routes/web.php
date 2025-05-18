1ebd67f135fefe01aa71a9818a6fe9f202667d6c
Jorge Manuel Bello <jorgebell@gmail.com>
2025-05-18 16:21:13 -0300

    Ajuste

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,6 @@ Route::middleware(['auth'])->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+//
 // Rutas de autenticación (Laravel Breeze)
 require __DIR__.'/auth.php';
